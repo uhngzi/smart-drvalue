ed2e98e143228011040bb9a667f52395738a7dbe
msyong <97266326+MisukYong@users.noreply.github.com>
2025-01-20 21:25:02 +0900

    fix: 오류 수정

--- src/pages/sayang/model/wait/form/[id].tsx
+++ src/pages/sayang/model/wait/form/[id].tsx
@@ -341,7 +341,7 @@ SayangModelAddPage.layout = (page: React.ReactNode) => (
   <MainPageLayout 
     menuTitle="모델 등록 및 현황"
     menu={[
-      {text:'모델 등록 대기', link:'/sayang/model/add'},
+      {text:'모델 등록 대기', link:'/sayang/model/wait'},
       {text:'모델 등록 현황', link:'/sayang/model/situation'},
     ]}
   >{page}</MainPageLayout>
