1833f7a462cab332082568684718a4c49475a2ac
msyong <97266326+MisukYong@users.noreply.github.com>
2025-02-16 00:52:57 +0900

    fix: 기본 값 변경

--- src/components/Input/AntdInputRound.tsx
+++ src/components/Input/AntdInputRound.tsx
@@ -24,7 +24,7 @@ const AntdInputRound: React.FC<Props> = ({
       $ht={styles?.ht?styles.ht:'30px'}
       $bg={styles?.bg?styles.bg:'none'}
       $bw={styles?.bw?styles.bw:'1px'}
-      $bc={styles?.bc?styles.bc:'#D5D5D5'}
+      $bc={styles?.bc?styles.bc:'#D9D9D9'}
       className={`${className}`}
     >
       <Input
