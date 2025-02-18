15f697afa03cb61166e6e78065e8873223beb2a0
msyong <97266326+MisukYong@users.noreply.github.com>
2025-02-19 03:48:46 +0900

    fix: 새로고침 없이 로드 될 수 있게 시도

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  
+  distDir: ".next_build",
 
   transpilePackages: [
     '@ant-design',
