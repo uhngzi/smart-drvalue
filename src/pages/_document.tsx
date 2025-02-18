8b771ed0d2bcdfcc4ede65ed37aa2294c9d3d257
msyong <97266326+MisukYong@users.noreply.github.com>
2025-02-19 03:56:19 +0900

    fix: 오류 삭제

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,14 +10,11 @@ import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
 import { ServerStyleSheet } from "styled-components";
 
 export default function MyDocument() {
-  const version = Date.now(); // 🔹 현재 타임스탬프 (버전 관리용)
-
   return (
     <Html lang="en">
       <Head>
-        {/* 🔹 Daum 주소 API에 버전 쿼리 추가 */}
         <script
-          src={`//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js?v=${version}`}
+          src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
           defer
         />
       </Head>
@@ -33,8 +30,8 @@ export default function MyDocument() {
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   const cache = createCache();
   const originalRenderPage = ctx.renderPage;
+
   const sheet = new ServerStyleSheet();
-  const version = Date.now(); // 🔹 버전 쿼리 추가
 
   ctx.renderPage = () =>
     originalRenderPage({
