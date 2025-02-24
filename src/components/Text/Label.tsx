b48447fe23df75ad79cfa9d5de665cd775274b4b
msyong <97266326+MisukYong@users.noreply.github.com>
2025-02-24 22:11:47 +0900

    feat: ClassName 추가

--- src/components/Text/Label.tsx
+++ src/components/Text/Label.tsx
@@ -1,30 +1,34 @@
 interface Props {
   label: string;
   icon?: React.ReactNode;
+  className?: string;
 }
 
 export const LabelThin:React.FC<Props> = ({
   label,
+  className,
 }) => {
   return (
-    <p className="h-22 text-[rgba(0, 0, 0, 0.85)]">{label}</p>
+    <p className={className+" h-22 text-[rgba(0, 0, 0, 0.85)]"} style={{ whiteSpace: 'pre-line' }}>{label}</p>
   )
 }
 
 export const LabelMedium:React.FC<Props> = ({
   label,
+  className,
 }) => {
   return (
-    <p className="text-[rgba(0, 0, 0, 0.85)] text-16 font-medium">{label}</p>
+    <p className={className+" text-[rgba(0, 0, 0, 0.85)] text-16 font-medium"} style={{ whiteSpace: 'pre-line' }}>{label}</p>
   )
 }
 
 export const LabelIcon:React.FC<Props> = ({
   label,
   icon,
+  className,
 }) => {
   return (
-    <div className="text-[rgba(0, 0, 0, 0.85)] h-center gap-4">
+    <div className={className+" text-[rgba(0, 0, 0, 0.85)] h-center gap-4"} style={{ whiteSpace: 'pre-line' }}>
       <p className="w-16 h-16 h-center justify-center">{icon}</p>
       {label}
     </div>
