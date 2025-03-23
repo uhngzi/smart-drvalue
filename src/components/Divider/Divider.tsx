72e7bb0575bf86548e28f5ad6313858da2251211
msyong <97266326+MisukYong@users.noreply.github.com>
2025-03-23 21:59:12 +0900

    feat: 클래스명 추가

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,7 +1,13 @@
+interface Props {
+  className?: string;
+}
+
 export const DividerH = () => {
   return <div className="w-full h-1 border-t-1"/>
 }
 
-export const DividerV = () => {
-  return <div className="w-1 h-full border-r-1"/>
+export const DividerV:React.FC<Props> = ({
+  className
+}) => {
+  return <div className={`w-1 h-full border-r-1 ${className}`}/>
 }
