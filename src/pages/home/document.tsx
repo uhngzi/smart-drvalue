9f865adb11161ce10bf086dff4ae306984f55be8
msyong <97266326+MisukYong@users.noreply.github.com>
2025-04-15 17:04:17 +0900

    fix: 샘플 페이지 추가

---
+++ src/pages/home/document.tsx
@@ -0,0 +1,41 @@
+import AntdModal from "@/components/Modal/AntdModal";
+import { TabLarge } from "@/components/Tab/Tabs";
+import MainPageLayout from "@/layouts/Main/MainPageLayout";
+import { Button, Empty } from "antd";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const HomeDocumentPage: React.FC & {
+  layout?: (page: React.ReactNode) => React.ReactNode;
+} = () => {
+  const router = useRouter();
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div className="h-[calc(100vh-192px)]">
+      <Button onClick={()=>{setOpen(true)}}>
+        임시
+      </Button>
+
+      <AntdModal
+        open={open}
+        setOpen={setOpen}
+        title={"미리보기"}
+        contents={
+          <>여기에 컴포넌트 선언하시면 됩니다</>
+        }
+        width={635}
+      />
+    </div>
+  )
+}
+
+HomeDocumentPage.layout = (page: React.ReactNode) => (
+  <MainPageLayout
+    menuTitle="샘플 페이지"
+  >
+    {page}
+  </MainPageLayout>
+);
+
+export default HomeDocumentPage;
