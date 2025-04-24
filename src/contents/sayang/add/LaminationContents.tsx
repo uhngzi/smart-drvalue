743a1e3ca33b719ee323a460d4979110c4713760
msyong <97266326+MisukYong@users.noreply.github.com>
2025-04-25 00:01:32 +0900

    fix: 적층구조 요소 라이브러리 에러 수정

--- src/contents/sayang/add/LaminationContents.tsx
+++ src/contents/sayang/add/LaminationContents.tsx
@@ -1,7 +1,7 @@
 import { SetStateAction, useEffect, useState } from "react";
 import { Button, Empty, Spin } from "antd";
 
-import Memo from '@/assets/svg/icons/memo.svg';
+import Memo from "@/assets/svg/icons/memo.svg";
 import MessageOn from "@/assets/svg/icons/message_on.svg";
 
 import { LayerEm } from "@/data/type/enum";
@@ -14,7 +14,7 @@ import AddLaminationModalContents from "./lamination/AddLaminationModalContents"
 import { specLaminationType } from "@/data/type/sayang/lamination";
 import { apiGetResponseType } from "@/data/type/apiResponse";
 import { useQuery } from "@tanstack/react-query";
-import { laminationRType } from "@/data/type/base/lamination";
+import { laminationSourceList } from "@/data/type/base/lamination";
 import { getAPI } from "@/api/get";
 import LaminationRow from "./lamination/LaminationRow";
 
@@ -36,52 +36,66 @@ const LaminationContents: React.FC<Props> = ({
   // 모달 창 띄우기
   const [open, setOpen] = useState<boolean>(false);
 
-  const [lamination, setLamination] = useState<laminationRType[]>([]);
+  const [lamination, setLamination] = useState<laminationSourceList[]>([]);
   const [submitFlag, setSubmitFlag] = useState<boolean>(false);
   const [lamNo, setLamNo] = useState<string>("");
 
   // ------------ 적층구조 구성요소 세팅 ----------- 시작
-  const [ baseLamination, setBaseLamination ] = useState<Array<laminationRType>>([]);
-  const { data:baseLaminationData, isLoading:baseLaminationLoading, refetch:baseLaminationRefetch } = useQuery<
-    apiGetResponseType, Error
-  >({
-    queryKey: ['lamination-source/jsxcrud/many'],
+  const [baseLamination, setBaseLamination] = useState<
+    Array<laminationSourceList>
+  >([]);
+  const {
+    data: baseLaminationData,
+    isLoading: baseLaminationLoading,
+    refetch: baseLaminationRefetch,
+  } = useQuery<apiGetResponseType, Error>({
+    queryKey: ["lamination-source/jsxcrud/many"],
     queryFn: async () => {
       const result = await getAPI({
-        type: 'baseinfo',
-        utype: 'tenant/',
-        url: 'lamination-source/jsxcrud/many'
+        type: "baseinfo",
+        utype: "tenant/",
+        url: "lamination-source/jsxcrud/many",
       });
 
       return result;
     },
   });
-  useEffect(()=>{
-    if(!baseLaminationLoading && baseLaminationData?.resultCode === 'OK_0000') {
+  useEffect(() => {
+    if (
+      !baseLaminationLoading &&
+      baseLaminationData?.resultCode === "OK_0000"
+    ) {
       setBaseLamination(baseLaminationData?.data?.data ?? []);
     }
-  }, [baseLaminationData])
+  }, [baseLaminationData]);
   // ------------ 적층구조 구성요소 세팅 ----------- 끝
 
   // ----------- 사양 적층구조 기본값 세팅 ---------- 시작
-  const { data:baseLaminationSourceData } = useQuery<
-    apiGetResponseType, Error
+  const { data: baseLaminationSourceData } = useQuery<
+    apiGetResponseType,
+    Error
   >({
-    queryKey: ['spec/lamination-source/jsxcrud/one'],
+    queryKey: ["spec/lamination-source/jsxcrud/one"],
     queryFn: async () => {
       const result = await getAPI({
-        type: 'core-d1',
-        utype: 'tenant/',
-        url: `spec/lamination-source/jsxcrud/one/${detailData.specLamination?.id}`
+        type: "core-d1",
+        utype: "tenant/",
+        url: `spec/lamination-source/jsxcrud/one/${detailData.specLamination?.id}`,
       });
 
-      if(result.resultCode === "OK_0000") {
-        const specLamination = result.data?.data?.specDetail?.data as {index:number, data:any, mappingResult: laminationRType}[];
-        let arr:laminationRType[] = [];
-        if(specLamination && specLamination.length > 0) {
-          specLamination.sort((a, b) => a.index - b.index).map((item) => {
-            arr.push(item.mappingResult);
-          })
+      if (result.resultCode === "OK_0000") {
+        const specLamination = result.data?.data?.specDetail?.data as {
+          index: number;
+          data: any;
+          mappingResult: laminationSourceList;
+        }[];
+        let arr: laminationSourceList[] = [];
+        if (specLamination && specLamination.length > 0) {
+          specLamination
+            .sort((a, b) => a.index - b.index)
+            .map((item) => {
+              arr.push(item.mappingResult);
+            });
         }
         setLamination(arr);
       }
@@ -92,54 +106,60 @@ const LaminationContents: React.FC<Props> = ({
   });
   // ----------- 사양 적층구조 기본값 세팅 ---------- 끝
 
-  useEffect(()=>{
-    if(submitFlag) {
+  useEffect(() => {
+    if (submitFlag) {
       handleSumbitTemp();
       setSubmitFlag(false);
     }
-  }, [submitFlag])
+  }, [submitFlag]);
 
   // 구성 요소에 따른 색상
-  const color = ['#CEE4B3','#F1F4F9','#7551E933','#F5D9B1','#F5B1A1'];
+  const color = ["#CEE4B3", "#F1F4F9", "#7551E933", "#F5D9B1", "#F5B1A1"];
 
   return (
     <div className="flex flex-col h-full gap-20">
       <div className="v-between-h-center">
-        <TitleIcon title="적층구조" icon={<MessageOn />}/>
-        { !view &&
-        <Button
-          className="h-32 rounded-6"
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          <p className="w-16 h-16">
-            <Memo/>
-          </p>
-          선택
-        </Button>
-        }
+        <TitleIcon title="적층구조" icon={<MessageOn />} />
+        {!view && (
+          <Button
+            className="h-32 rounded-6"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <p className="w-16 h-16">
+              <Memo />
+            </p>
+            선택
+          </Button>
+        )}
       </div>
-      { (detailData.specLamNo ?? detailData.specLamination?.lamNo ?? lamNo) &&
-      <div className="w-full flex v-between-h-center h-24 text-14">
-        <span>코드 : {detailData.specLamNo ?? detailData.specLamination?.lamNo ?? lamNo}</span>
-      </div>}
+      {(detailData.specLamNo ?? detailData.specLamination?.lamNo ?? lamNo) && (
+        <div className="w-full flex v-between-h-center h-24 text-14">
+          <span>
+            코드 :{" "}
+            {detailData.specLamNo ?? detailData.specLamination?.lamNo ?? lamNo}
+          </span>
+        </div>
+      )}
 
       <div className="flex-1 text-12 text-[#292828] flex flex-col gap-3">
-        { Array.isArray(lamination) && lamination.length > 0 &&
-          lamination.map((item:laminationRType, index:number) => (
+        {Array.isArray(lamination) &&
+          lamination.length > 0 &&
+          lamination.map((item: laminationSourceList, index: number) => (
             <LaminationRow
-              key={item.id+':'+index}
+              key={item.id + ":" + index}
               item={item}
               index={index}
               color={color}
             />
-          ))
-        }
-        { (!Array.isArray(lamination) || lamination?.length < 1) &&
-          <div className="w-full h-full v-h-center"><Empty /></div>}
+          ))}
+        {(!Array.isArray(lamination) || lamination?.length < 1) && (
+          <div className="w-full h-full v-h-center">
+            <Empty />
+          </div>
+        )}
       </div>
-
 
       <AntdModal
         open={open}
@@ -147,25 +167,26 @@ const LaminationContents: React.FC<Props> = ({
         draggable={true}
         title={"적층구조 라이브러리 선택 및 편집 구성"}
         contents={
-        <AddLaminationModalContents
-          defaultLayerEm={defaultLayerEm}
-          detailData={detailData}
-          setDetailData={setDetailData}
-          handleSumbitTemp={handleSumbitTemp}
-          baseLamination={baseLamination}
-          baseLaminationLoading={baseLaminationLoading}
-          baseLaminationRefetch={baseLaminationRefetch}
-          color={color}
-          mainLamination={lamination}
-          setMainLamination={setLamination}
-          submitFlag={submitFlag}
-          setSubmitFlag={setSubmitFlag}
-          setLamNo={setLamNo}
-        />}
+          <AddLaminationModalContents
+            defaultLayerEm={defaultLayerEm}
+            detailData={detailData}
+            setDetailData={setDetailData}
+            handleSumbitTemp={handleSumbitTemp}
+            baseLamination={baseLamination}
+            baseLaminationLoading={baseLaminationLoading}
+            baseLaminationRefetch={baseLaminationRefetch}
+            color={color}
+            mainLamination={lamination}
+            setMainLamination={setLamination}
+            submitFlag={submitFlag}
+            setSubmitFlag={setSubmitFlag}
+            setLamNo={setLamNo}
+          />
+        }
         width={1044}
       />
     </div>
-  )
-}
+  );
+};
 
 export default LaminationContents;
