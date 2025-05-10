5dbc2f144c0c4a3a739ab7b9900d9f43a13e20f2
msyong <97266326+MisukYong@users.noreply.github.com>
2025-05-10 18:19:05 +0900

    fix: 전화번호 형식 추가 및 영문 체크 추가

--- src/components/List/CardInputList.tsx
+++ src/components/List/CardInputList.tsx
@@ -14,6 +14,7 @@ import styled from "styled-components";
 import { useQuery } from "@tanstack/react-query";
 import { getPrtCsAPI } from "@/api/cache/client";
 import { partnerRType } from "@/data/type/base/partner";
+import { isValidEnglish } from "@/utils/formatEnglish";
 
 interface Item {
   name: string;
@@ -209,13 +210,23 @@ const CardInputList: React.FC<CardInputListProps> = ({
                   ) : // 전화번호 형식 체크
                   (item.name.toLowerCase().includes("tel") ||
                       item.name.toLowerCase().includes("mobile") ||
-                      item.name === "taxManagerPhone") &&
+                      item.name.toLowerCase().includes("phone")) &&
                     !isValidTel(item?.value?.toString()) ? (
                     <div className="h-center gap-3 text-[red]">
                       <p className="w-15 h-15">
                         <Hint />
                       </p>
                       올바르지 않은 전화번호입니다.
+                    </div>
+                  ) : // 영문 입력 체크
+                  (item.name.toLowerCase().includes("enm") ||
+                      item.name.toLowerCase().includes("eng")) &&
+                    !isValidEnglish(item?.value?.toString()) ? (
+                    <div className="h-center gap-3 text-[red]">
+                      <p className="w-15 h-15">
+                        <Hint />
+                      </p>
+                      영문 또는 숫자만 입력 가능합니다.
                     </div>
                   ) : (
                     <></>
